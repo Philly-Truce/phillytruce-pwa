b143292f43e5d97d2aae18a46cfc726a0b18595b
divya-prav <divya.praveen.5215@gmail.com>
2024-07-31 13:35:43 -0500

    Added-create form field

--- src/app/create-report/page.tsx
+++ src/app/create-report/page.tsx
@@ -5,16 +5,16 @@ export default function createReport() {
   return (
     <div>
       <div className="flex flex-row bg-primary text-white h-[64px] justify-start items-center p-[16px] gap-4">
-        <GoArrowLeft />
-        <h1>Create New Report</h1>
+        <GoArrowLeft size={25}/>
+        <h1 className="text-xl">Create New Report</h1>
       </div>
       <div className="px-5 py-3">
         <p className="text-sm py-2">Please provide detailed information of the incident in this form</p>
         <hr/>
         <NewReportForm />
-        <div className="flex flex-col gap-2">
-        <button className="bg-primary rounded-full text-white py-1">Save</button>
-        <button className="bg-primary rounded-full text-white py-1">Cancel</button>
+        <div className="flex flex-col gap-2 py-8 sticky">
+        <button className="bg-primary rounded-xl text-white text-sm py-2">SAVE</button>
+        <button className="rounded-xl text-primary text-sm py-2 p-1 border border-black">CANCEL</button>
         </div>
       </div>
     </div>
