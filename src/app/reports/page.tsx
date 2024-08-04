be3f12076b6b7658d38b3aff723ef0f2163f89c3
Gaurav Varma <gaurav.varma@hey.com>
2024-08-04 14:07:20 -0700

    refactor(reports): add id tags and convert to flexbox layout

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -5,18 +5,20 @@ import SearchBar from "@/components/search-bar";
 
 export default function Main() {
   return (
-    <div className="overflow-y-scroll">
+    <div
+      id="reports-page-container"
+      className="overflow-y-scroll flex flex-col gap-4"
+    >
       <SearchBar />
       <List />
       <div id="new-report-button" className="bottom-4 right-4 float-end">
         <Link href="/create">
-          <button className="flex items-center gap-2 p-4 bg-primary text-white rounded-xl shadow-[rgba(50,50,93,0.25)_0px_13px_27px_-5px,rgba(0,0,0,0.3)_0px_8px_16px_-8px]">
+          <button className="flex items-center gap-2 p-4 bg-primary text-white rounded-xl shadow-[rgba(50,50,93,0.25)_0px_13px_27px_-5px,rgba(0,0,0,0.3)_0px_8px_16px_-8px] ml-auto">
             <IoMdAdd />
             New Report
           </button>
         </Link>
       </div>
-      {/*  */}
     </div>
   );
 }
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -5,7 +5,7 @@ export default function SearchBar() {
   return (
     <div
       id="search-bar"
-      className="flex items-center justify-between bg-accent rounded-[1.75rem] p-2 my-4"
+      className="flex items-center justify-between bg-accent rounded-[1.75rem] p-2"
     >
       <div className="flex items-center gap-5">
         <IoSearchSharp />
