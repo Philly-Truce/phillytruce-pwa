7a73eacd02520d92802b91adc39d9e8121c82caa
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-06 17:36:20 -0400

    fixed button alignment ....circle back to see if flex box can do this instead of gap

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -7,11 +7,13 @@ import AnalyticsIcon from "@/assets/icons/analytics-icon";
 import SettingsIcon from "@/assets/icons/settings-icon";
 import HelpIcon from "@/assets/icons/help-icon";
 import Divider from "@/assets/icons/divider";
+import Link from "next/link";
 
 
 interface Options {
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
   option: string;
+  onClick?: () => void;
 }
 
 const MenuOption = ({ icon, option }: Options) => {
@@ -30,18 +32,19 @@ const MenuOption = ({ icon, option }: Options) => {
 }
 
 export default function MenuDrawer() {
+  
   return (
     <div className="bg-white text-black">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-44">
         <ul>
           <MenuOption icon={<ProfileIcon />} option="Profile" />
           <MenuOption icon={<PhoneIcon />} option="Contacts" />
           <MenuOption icon={<ResourcesIcon />} option="Resources" />
           <MenuOption icon={<AnalyticsIcon />} option="Analytics" />
-          <MenuOption icon={<SettingsIcon />} option="Settings" />
+          <Link href="/messages"><MenuOption icon={<SettingsIcon />} option="Settings" /></Link>
           <MenuOption icon={<HelpIcon />} option="Help Center" />
         </ul>
-        <button className="mt-32 items-center bg-[#1C4587] rounded-full  text-white w-full py-3">
+        <button className="items-center bg-[#1C4587] rounded-full  text-white w-full py-3">
         Log Out
       </button>
       </div>
