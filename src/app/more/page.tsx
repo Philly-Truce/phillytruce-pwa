48e3103b308a88964da6bbdd81a43fdbe144c22b
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-06 16:51:46 -0400

    add icons and fixed css

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -1,32 +1,52 @@
 import React from "react";
+import ProfileIcon from "@/assets/icons/profile-icon";
+import PhoneIcon from "@/assets/icons/phone-icon";
+import ResourcesIcon from "@/assets/icons/resources-icon";
+import RightArrow from "@/assets/icons/right-arrow";
+import Divider from "@/assets/icons/Divider";
+import AnalyticsIcon from "@/assets/icons/analytics-icon";
+import SettingsIcon from "@/assets/icons/settings-icon";
+
+
+interface Options {
+  icon: React.ReactNode;
+  option: string;
+}
+
+const MenuOption = ({ icon, option }: Options) => {
+  return (
+    <>
+
+      <div className="flex p-5">
+        <div className="w-3/4 flex justify-start items-center">
+          <div className="mr-3">{icon}</div>{option}</div>
+        <div className="w-1/4  flex justify-end items-center">
+          <RightArrow /></div>
+      </div>
+      <Divider />
+    </>
+  )
+}
 
 export default function MenuDrawer() {
-  const menuOptions = [
-    { icon: "@", option: "Profile" },
-    { icon: "@", option: "Contacts" },
-    { icon: "@", option: "Resources" },
-    { icon: "@", option: "Analytics" },
-    { icon: "@", option: "Settings" },
-    { icon: "@", option: "Help Center" },
-  ];
-
   return (
-    <div className="bg-white text-black h-[70vh] min-w-[384px] absolute pt-[16px] px-[16px] justify-between items-center">
-      <div className="flex flex-col gap-[12px]">
+    <div className="bg-white text-black">
+      <div className="flex flex-col">
         <ul>
-          {menuOptions.map((item) => {
-            return (
-              <li className=" h-[50px] pt-[8px] pr-[12px]" key={item.option}>
-                {item.icon}
-                {item.option}
-              </li>
-            );
-          })}
+          <MenuOption icon={<ProfileIcon />} option="Profile" />
+          <MenuOption icon={<PhoneIcon />} option="Contacts" />
+          <MenuOption icon={<ResourcesIcon />} option="Resources" />
+          <MenuOption icon={<AnalyticsIcon />} option="Analytics" />
+          <MenuOption icon={<SettingsIcon />} option="Settings" />
+          <MenuOption icon={<ProfileIcon />} option="Help Center" />
         </ul>
-      </div>
-      <button className="flex flex-col px-[10px] py-[15px] justify-center items-center w-full bg-[#1C4587] rounded-full text-white mt-40">
+        <button className="mt-32 items-center bg-[#1C4587] rounded-full  text-white w-full py-3">
         Log Out
       </button>
-    </div>
+      </div>
+     
+     
+      </div>
+    
   );
 }
