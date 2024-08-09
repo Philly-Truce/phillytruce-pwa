ccc42f6354e0d2b9842cc1571f8ea7b16c6db77c
Gaurav Varma <gaurav.varma@hey.com>
2024-08-08 20:30:48 -0700

    added tailwind css animation in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        slide: {
+          from: {
+            transform: "translateX(100%)",
+          },
+          to: {
+            transform: "translateX(0%)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        slide: "slide 750ms ease-in-out",
       },
     },
   },
