c9ab0c3cb8e0d2d9dd060ba65e37af8a50e618f2
fuguoxue-nyu <144282221+fuguoxue-nyu@users.noreply.github.com>
2024-08-19 16:32:58 +0800

    Finished sign in authorization function

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,35 @@
 import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "Username"
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password"
+        }
+      },
+      async authorize(credentials) {
+        // Retrieve user data to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = {id: "42", name: "Dave", password: "phillytruce"}
+
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user;
+        } else {
+          return null;
+        }
+      }
+    })
+  ],
+  
 };
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import { options } from "./options";
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-  ],
-};
-
-const handler = async (req: NextRequest) => {
-  const { headers, body } = req;
-  const nextAuthReq = {
-    ...req,
-    headers: Object.fromEntries(headers),
-    body: await req.text(),
-  };
-
-  const res = NextResponse.next();
-
-  await NextAuth(nextAuthReq as any, res as any, authOptions);
-
-  return res;
-};
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
