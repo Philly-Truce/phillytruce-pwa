f3894ffcc9fb71b274f9d85c8a5921d1327eb5aa
divya-prav <divya.praveen.5215@gmail.com>
2024-09-04 10:26:50 -0500

    Added-Width to full screen

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,7 @@ import ReportForm from "@/components/create-report/report-form";
 
 export default function createReport() {
   return (
-    <div>
+    <div className="w-full">
       <ReportForm report=''/>
     </div>
   );
