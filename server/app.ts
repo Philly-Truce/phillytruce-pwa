b43d176a5a9e7a0585828eb85fe2498d8cb92bd5
John Jang <jscolbe9675@gmail.com>
2024-09-12 13:56:17 -0400

    retry config #4

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import express from "express";
 import "express-async-errors";
 import Cors from "cors";
 import morgan from "morgan";
